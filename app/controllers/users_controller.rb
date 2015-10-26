b8b8540fd47314fdf0049144849f2ae5d6dfeed8
itotamami <ito@exmedia.jp>
2015-10-26 05:59:00 +0000

    Lesson7 9 follower task2 temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,42 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+ # Added the above lines  L7.6.5
 
+ # Added the below lines  L7.7.task1
+  def edit
+       @user=User.find(params[:id])
+  end
+
+  def update
+      @user=User.find(params[:id])
+    if @user.update(user_params)
+        flash[:success] =  "Updated your profile!"
+       redirect_to @user 
+    else
+      render 'edit'
+    end
+    
+  end
+ # Added the above lines  L7.7.task1
+
+
+ # Added the below lines  L7.9.task2
+ def followings
+  #フォローしているユーザーを表示するメソッド
+
+ end
+ 
+ def followers
+  #フォローされているユーザーを表示するメソッド
+
+ end
+
+ # Added the above lines  L7.9.task2
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:area,:profile)
   end
 end
